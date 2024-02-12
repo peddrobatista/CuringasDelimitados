01096efa238c93a8fa907f92da620675e0e45406
pedro-batista <peddrobatista2022@gmail.com>
2024-02-12 17:57:55 -0300

    Primeiro commit...

---
+++ src/application/Program.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Circle;
+import entities.Rectangle;
+import entities.Shape;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		
+		List<Shape> myShapes = new ArrayList<>();
+		
+		myShapes.add(new Rectangle(3.0, 2.0));
+		myShapes.add(new Circle(2.0));
+		
+		System.out.println("Total area: " + totalArea(myShapes));
+	}
+	
+	public static double totalArea(List<Shape> list) {
+		double sum = 0.0;
+		for(Shape s : list) {
+			sum += s.area();
+		}
+		
+		return sum;
+	}
+
+}
