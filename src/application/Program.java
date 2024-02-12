79658bd2cc3df40e70c10d719cf583e9cead950b
pedro-batista <peddrobatista2022@gmail.com>
2024-02-12 18:18:57 -0300

    Primeiro commit...

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,34 +3,17 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 
-import entities.Circle;
-import entities.Rectangle;
-import entities.Shape;
-
 public class Program {
 	
 	public static void main(String[] args) {
 		
-		List<Shape> myShapes = new ArrayList<>();
+		List<Integer> intList = new ArrayList<Integer>();
+		intList.add(10);
+		intList.add(5);
+		List<? extends Number> list = intList;
+		Number x = list.get(0);
+		list.add(20); // erro de compilacao
 		
-		myShapes.add(new Rectangle(3.0, 2.0));
-		myShapes.add(new Circle(2.0));
-		
-		List<Circle> myCircles = new ArrayList<>();
-		
-		myCircles.add(new Circle(2.0));
-		myCircles.add(new Circle(2.0));
-		
-		System.out.println("Total area: " + totalArea(myCircles));
-	}
-	
-	public static double totalArea(List<? extends Shape> list) {
-		double sum = 0.0;
-		for(Shape s : list) {
-			sum += s.area();
-		}
-		
-		return sum;
 	}
 
 }
