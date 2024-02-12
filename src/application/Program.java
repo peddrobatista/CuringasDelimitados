6acc64ea84ef57998686b77af27ac7b216f7a517
pedro-batista <peddrobatista2022@gmail.com>
2024-02-12 18:02:41 -0300

    Segundo commit...

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,10 +16,15 @@ public class Program {
 		myShapes.add(new Rectangle(3.0, 2.0));
 		myShapes.add(new Circle(2.0));
 		
-		System.out.println("Total area: " + totalArea(myShapes));
+		List<Circle> myCircles = new ArrayList<>();
+		
+		myCircles.add(new Circle(2.0));
+		myCircles.add(new Circle(2.0));
+		
+		System.out.println("Total area: " + totalArea(myCircles));
 	}
 	
-	public static double totalArea(List<Shape> list) {
+	public static double totalArea(List<? extends Shape> list) {
 		double sum = 0.0;
 		for(Shape s : list) {
 			sum += s.area();
